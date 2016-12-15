d81747885cc68086e1d7ae41fbb6d1fa9dc2d9f8
Jérémie Ledentu <jledentu@gmail.com>
2016-12-15 16:04:27 +0100

    Test app generator

---
+++ test/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const path = require('path');
+const helpers = require('yeoman-test');
+const assert = require('yeoman-assert');
+
+/**
+ * Test app generator.
+ */
+describe('app', () => {
+
+  describe('default settings', () => {
+    before(done => {
+      this.generator = helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .toPromise()
+        .then(() => done());
+    });
+
+    it('should generate package.json file', () => {
+      assert.file('package.json');
+    });
+
+    it('should generate webpack.config.js file', () => {
+      assert.file('webpack.config.js');
+    });
+
+    it('should generate print-pdf script file', () => {
+      assert.file('scripts/print-pdf.js');
+    });
+
+    it('should generate index file', () => {
+      assert.file('src/index.ejs');
+    });
+
+    it('should generate JavaScript source files', () => {
+      assert.file('src/js/main.js');
+    });
+
+    it('should generate SASS source files', () => {
+      assert.file('src/scss/main.scss');
+    });
+
+    it('should generate slides files', () => {
+      assert.file('src/slides/title.md');
+    });
+  });
+
+});
